95eb147f5e19c69ac703d38d8b019a83d61c86ef
matthu017 <56143351+matthu017@users.noreply.github.com>
2021-11-06 22:15:14 -0400

    First commit

---
+++ veeva-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/Table';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
